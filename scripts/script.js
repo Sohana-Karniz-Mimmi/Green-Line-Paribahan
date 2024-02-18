911a5671e2779d0577c76f424965b7edb39e0561
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2024-02-18 13:44:07 +0600

    seat countting done

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,22 @@ const buyTicketsBtn = document.getElementById('buy-tickets').addEventListener('c
 
 
 // Select Your Seat
+const seatBtn = document.getElementsByClassName('seat-btn')
+let count = 0;
+    for(let singleSeat of seatBtn){
+        const index = singleSeat
+        index.addEventListener('click', function(event){
+            //Set Seat Button background color and disabled
+            event.target.classList.remove('bg-[#F7F8F8]')
+            event.target.classList.add('bg-[#1DD100]', 'text-white');
+            event.target.setAttribute("disabled", true);
+
+            // Seat Count
+            const seatCount = document.getElementById('seat-count')
+                seatCount.innerText = count += 1;
+            let seatLeft = document.getElementById('seat-left').innerText
+            let convertedNumber = parseInt(seatLeft);
+            const number = document.getElementById('seat-left').innerText = convertedNumber -= 1;
+            
+       })
+    }
---
+++ scripts/utility.js
@@ -0,0 +1,7 @@
+// set seat Button background color 
+// function setBackgroundColor(backgroundColor){
+//     const background = document.querySelectorAll(backgroundColor);
+//     console.log(background);
+//     background.classList.remove('bg-[#F7F8F8]')
+//     // background.classList.add('bg-[#1DD100]');
+// }
