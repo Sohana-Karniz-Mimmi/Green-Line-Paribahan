5f82a18e2536aaf9e278952a7b25120dac33ed3c
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2024-02-18 11:19:36 +0600

    Buy Tickets button to seat section scrolling done

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+// For Scrolling Buy Tickets button to seat section
+const buyTicketsBtn = document.getElementById('buy-tickets').addEventListener('click', function(){
+    const seatSection = document.getElementById('seat -section').scrollIntoView({ behavior: 'smooth' })
+})
+
+
