45f4388df586d1087c51ffef737b2f32a557e399
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2024-02-18 14:17:32 +0600

    Add seat number and price

--- scripts/script.js
+++ scripts/script.js
@@ -21,6 +21,27 @@ let count = 0;
             let seatLeft = document.getElementById('seat-left').innerText
             let convertedNumber = parseInt(seatLeft);
             const number = document.getElementById('seat-left').innerText = convertedNumber -= 1;
+
+            // Add seat number and price
+            const addSeatContainer = document.getElementById('add-seat-container')
+                addSeatContainer.classList.add('border-b', 'border-solid', 'border-[#13131833]')
+            const div = document.createElement('div')
+                div.classList.add('flex', 'justify-between', 'items-center', 'pb-3')
+                addSeatContainer.appendChild(div);
+
+            const value = event.target.innerText
+            const p1 = document.createElement('p')
+             p1.classList.add('font-inter', 'font-semibold', 'text-[16px]', 'text-[#03071299]')
+             p1.innerText = value;
+            div.appendChild(p1)
+            const p2 = document.createElement('p')
+             p2.classList.add('font-inter', 'font-semibold', 'text-[16px]', 'text-[#03071299]')
+             p2.innerText = 'Economoy';
+            div.appendChild(p2)
+            const p3 = document.createElement('p')
+             p3.classList.add('font-inter', 'font-semibold', 'text-[16px]', 'text-[#03071299]')
+             p3.innerText = 550;
+            div.appendChild(p3)
             
        })
     }
